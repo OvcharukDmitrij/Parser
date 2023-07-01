60fd5064c98724e35b17f1e5f075f84297be49a0
Дмитрий Овчарук <ovcharukdmitrij@yandex.ru>
2023-07-01 22:08:16 +0300

    Осталось пройти один тест и написать Readme

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,33 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from collections import defaultdict
+from datetime import datetime
+# from pathlib import Path
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+# BASE_DIR = Path(__file__).parent.parent
+# DIR_OUTPUT = 'results'
 
 
 class PepParsePipeline:
+    def open_spider(self, spider):
+        # Создаем словарь-счетчик статусов
+        self.counter_status = defaultdict(int)
+
     def process_item(self, item, spider):
+        # Подсчитываем количество статусов
+        self.counter_status[item['status']] += 1
         return item
+
+    def close_spider(self, spider):
+        # Определяем текущую дату и время
+        current_date = "{:%Y-%m-%d_%H-%M-%S}".format(datetime.now())
+        # Создаем файл в формате csv
+        with open(
+                f'results/status_summary_{current_date}.csv',
+                mode='w',
+                encoding='utf-8'
+        ) as f:
+            # Заполняем таблицу данными из счетчика статусов
+            f.write('Статус,Количество\n')
+            for status, num in self.counter_status.items():
+                f.write(f'{status},{num}\n')
+            # Добавляем строчку с общим количеством pep
+            f.write(f'Total,{sum(self.counter_status.values())}\n')
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -3,28 +3,32 @@ import scrapy
 from pep_parse.items import PepParseItem
 
 
+# Класс с созданным пауком
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
+        # Создаем селектор со сылками на все pep
         table_numerical_index = response.css(
             'table.pep-zero-table.docutils.align-default'
         )
         all_peps = table_numerical_index.css(
             'a.pep.reference.internal::attr(href)'
         )
+        # Запускаем парсинг страницы каждого pep из общей таблицы
         for pep_link in all_peps:
             yield response.follow(pep_link, callback=self.parse_pep)
 
     def parse_pep(self, response):
+        # Создаем селектор для поиска всех нужных сведений о каждом pep
         title_page = response.css('title::text').get()
         title = title_page.strip('PEP | peps.python.org')
         number_and_name = title.split()
         number = number_and_name[0]
         name = ' '.join(number_and_name[2:])
-
+        # Создаем словарь с найденными сведениями
         data = {
             'number': number,
             'name': name,
@@ -32,4 +36,5 @@ class PepSpider(scrapy.Spider):
                 'dt:contains("Status") + dd abbr::text'
             ).get()
         }
+        # Возвращаем экземпляр Item для конкретного pep
         yield PepParseItem(data)
